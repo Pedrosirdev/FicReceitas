1552dc5f862a515a1dc2cab8008aef72f6dee108
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-03 00:07:56 -0300

    adicionando javascript ao menu offcanvas

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,10 +1,29 @@
 export {navbar}
 
 const btnMenu = document.querySelector('#btn-menu')
+const btnClose = document.querySelector('#btn-close')
+const navbarOffcanvas = document.querySelector('#navbar-offcanvas')
+const logo = document.querySelector('#logo')
 
 function navbar(){
   btnMenu.addEventListener('click',(ev) => {
     ev.preventDefault()
-    alert('aaaaaaaaa')
+      btnMenu.classList.add('none')
+      btnClose.classList.remove('none')
+      navbarOffcanvas.classList.remove('hidden-offcanvas')
+      navbarOffcanvas.classList.add('show-offcanvas')
+      logo.classList.add('none')
   })
+
+  btnClose.addEventListener('click',(ev) => {
+    ev.preventDefault()
+    btnMenu.classList.remove('none')
+    btnClose.classList.add('none')
+    navbarOffcanvas.classList.add('hidden-offcanvas')
+    navbarOffcanvas.classList.remove('show-offcanvas')
+    logo.classList.remove('none')
+
+  })
+
+
 }
