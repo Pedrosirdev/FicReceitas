33470716caec002262ee18113d733c49d51470fe
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-05 23:03:14 -0300

    Criação do caurosel, adicionando javascript a ele, quase finalizado

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { navbar, searchBarIcon } from "./javascript/navbar.js";
+import { globalCarousel } from "./javascript/carousel.js";
 
 navbar()
 searchBarIcon()
+globalCarousel()
+
+
 
---
+++ javascript/carousel.js
@@ -0,0 +1,67 @@
+export { globalCarousel }
+
+
+const radio1 = document.querySelector('#radio1')
+const autoBtn1 = document.querySelector('.auto-btn1')
+const radio2 = document.querySelector('#radio2')
+const autoBtn2 = document.querySelector('.auto-btn2')
+const radio3 = document.querySelector('#radio3')
+const autoBtn3 = document.querySelector('.auto-btn3')
+const radio4 = document.querySelector('#radio4')
+const autoBtn4 = document.querySelector('.auto-btn4')
+
+// const radios = [ radio1, radio2, radio3, radio4 ]
+// const btns = [ autoBtn1, autoBtn2, autoBtn3, autoBtn4 ]
+
+function globalCarousel() {
+
+  function check(){
+    radio1.addEventListener('click', () => {
+      autoBtn1.classList.add('background-btn-auto')
+      autoBtn2.classList.remove('background-btn-auto')
+      autoBtn3.classList.remove('background-btn-auto')
+      autoBtn4.classList.remove('background-btn-auto')
+    })
+    radio2.addEventListener('click', () => {
+      autoBtn2.classList.add('background-btn-auto')
+      autoBtn1.classList.remove('background-btn-auto')
+      autoBtn3.classList.remove('background-btn-auto')
+      autoBtn4.classList.remove('background-btn-auto')
+    })
+    radio3.addEventListener('click', () => {
+      autoBtn3.classList.add('background-btn-auto')
+      autoBtn2.classList.remove('background-btn-auto')
+      autoBtn1.classList.remove('background-btn-auto')
+      autoBtn4.classList.remove('background-btn-auto')
+    })
+    radio4.addEventListener('click', () => {
+      autoBtn4.classList.add('background-btn-auto')
+      autoBtn2.classList.remove('background-btn-auto')
+      autoBtn3.classList.remove('background-btn-auto')
+      autoBtn1.classList.remove('background-btn-auto')
+    })
+  }
+
+  check()
+  
+  let count = 1
+  document.getElementById("radio1").checked = true
+
+  setInterval(() => {
+    nextImage()
+  }, 5000);
+
+  function nextImage() {
+    count++
+    if(count>4){
+      count = 1
+    }
+  
+    document.getElementById("radio"+count).checked = true
+  }
+
+  nextImage()
+
+}
+
+
