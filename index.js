d16c52ee4bf6034ea15cdecca4cfb15d911aeea3
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-08 10:50:40 -0300

    Finalizção do javascript do carousel

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import { navbar, searchBarIcon } from "./javascript/navbar.js";
-// import { check, nextImage } from "./javascript/carousel.js";
+import { globalFunctionCarousel } from "./javascript/carousel.js";
 
 navbar()
 searchBarIcon()
 
 // carousel.js
-
-// check()
-// radio1.checked = true
-
-// setInterval(() => {
-//   nextImage()
-// }, 5000);
-
+globalFunctionCarousel()
 
 
 
--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,4 +1,4 @@
-export { check, nextImage }
+export { globalFunctionCarousel }
 
 
 const radio1 = document.querySelector('#radio1')
@@ -10,64 +10,52 @@ const autoBtn3 = document.querySelector('.auto-btn3')
 const radio4 = document.querySelector('#radio4')
 const autoBtn4 = document.querySelector('.auto-btn4')
 
-  function check(){
-    radio1.addEventListener('click', () => {
-      autoBtn1.classList.add('background-btn-auto')
-      autoBtn2.classList.remove('background-btn-auto')
-      autoBtn3.classList.remove('background-btn-auto')
-      autoBtn4.classList.remove('background-btn-auto')
-    })
-    radio2.addEventListener('click', () => {
-      autoBtn2.classList.add('background-btn-auto')
-      autoBtn1.classList.remove('background-btn-auto')
-      autoBtn3.classList.remove('background-btn-auto')
-      autoBtn4.classList.remove('background-btn-auto')
-    })
-    radio3.addEventListener('click', () => {
-      autoBtn3.classList.add('background-btn-auto')
-      autoBtn2.classList.remove('background-btn-auto')
-      autoBtn1.classList.remove('background-btn-auto')
-      autoBtn4.classList.remove('background-btn-auto')
-    })
-    radio4.addEventListener('click', () => {
-      autoBtn4.classList.add('background-btn-auto')
-      autoBtn2.classList.remove('background-btn-auto')
-      autoBtn3.classList.remove('background-btn-auto')
-      autoBtn1.classList.remove('background-btn-auto')
-    })
-  }
-  let count = 1
-  function nextImage() {
-    count++
-    if(count>4){
-      count = 1
+  function globalFunctionCarousel() {
+    function check(){
+      radio1.addEventListener('click', () => {
+        autoBtn1.classList.add('background-btn-auto')
+        autoBtn2.classList.remove('background-btn-auto')
+        autoBtn3.classList.remove('background-btn-auto')
+        autoBtn4.classList.remove('background-btn-auto')
+      })
+      radio2.addEventListener('click', () => {
+        autoBtn2.classList.add('background-btn-auto')
+        autoBtn1.classList.remove('background-btn-auto')
+        autoBtn3.classList.remove('background-btn-auto')
+        autoBtn4.classList.remove('background-btn-auto')
+      })
+      radio3.addEventListener('click', () => {
+        autoBtn3.classList.add('background-btn-auto')
+        autoBtn2.classList.remove('background-btn-auto')
+        autoBtn1.classList.remove('background-btn-auto')
+        autoBtn4.classList.remove('background-btn-auto')
+      })
+      radio4.addEventListener('click', () => {
+        autoBtn4.classList.add('background-btn-auto')
+        autoBtn2.classList.remove('background-btn-auto')
+        autoBtn3.classList.remove('background-btn-auto')
+        autoBtn1.classList.remove('background-btn-auto')
+      })
     }
-    document.getElementById("radio"+count).checked = true
-    
-    
-  }
 
-  if(document.getElementById("radio4").checked = true){
-    autoBtn1.classList.add('background-btn-auto')
-    // autoBtn2.classList.remove('background-btn-auto')
-    // autoBtn3.classList.remove('background-btn-auto')
-    // autoBtn4.classList.remove('background-btn-auto')
-  }
-  if(document.getElementById("radio3").checked = true){
-    autoBtn2.classList.add('background-btn-auto')
-    // autoBtn1.classList.remove('background-btn-auto')
-    // autoBtn3.classList.remove('background-btn-auto')
-    // autoBtn4.classList.remove('background-btn-auto')
-  }
-  if(document.getElementById("radio2").checked = true){
-    autoBtn3.classList.add('background-btn-auto')
-    // autoBtn2.classList.remove('background-btn-auto')
-    // autoBtn1.classList.remove('background-btn-auto')
-    // autoBtn4.classList.remove('background-btn-auto')
-  }
-  if(document.getElementById("radio1").checked = true){
-    autoBtn4.classList.add('background-btn-auto')
-    // autoBtn3.classList.remove('background-btn-auto')
-    autoBtn1.classList.remove('background-btn-auto')
-    // autoBtn2.classList.remove('background-btn-auto')
+    check()
+
+    let count = 1
+    radio1.checked = true
+
+    setInterval(() => {
+      nextImage()
+    }, 5000);
+
+    function nextImage() {
+      count++
+      if(count>4){
+        count = 1
+      }
+      document.getElementById("radio"+count).checked = true
+      
+      
+    }
+
+    nextImage()
   }
