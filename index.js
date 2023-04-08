490d6ce14a96ff8ce3e4e1ae2cfca0c5fa2a066b
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-07 23:56:39 -0300

    Salvamento de rotina

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { navbar, searchBarIcon } from "./javascript/navbar.js";
-import { check, nextImage } from "./javascript/carousel.js";
+// import { check, nextImage } from "./javascript/carousel.js";
 
 navbar()
 searchBarIcon()
 
 // carousel.js
-check()
-radio1.checked = true
 
-setInterval(() => {
-  nextImage()
-}, 5000);
+// check()
+// radio1.checked = true
+
+// setInterval(() => {
+//   nextImage()
+// }, 5000);
 
 
 
