bcdc4e035290e08df7f2b29bfb167c70e913c9f4
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-23 11:02:47 -0300

    Iniciando implementação do mecanismo de busca de receitas

--- javascript/searchFunction.js
+++ javascript/searchFunction.js
@@ -1,13 +1,36 @@
 
-const searchInput = document.querySelectorAll(".input-search")
+const searchInput = document.querySelector(".input-search")
+
+searchInput.addEventListener('input', search)
+
+function search(){
+  const query = searchInput.value.toLowerCase()
+
+  const pages = {
+    'Pão de queijo': '../pages/recipes/cheese-bread.html',
+    'cheese bread': '../pages/recipes/cheese-bread.html',
+
+    'filé de peixe': '../pages/recipes/fish-fillet.html',
+    'fish fillet': '../pages/recipes/fish-fillet.html'
+  }
+  const url = pages[query]
+
+  if(url){
+    window.location.href = url
+  } else{
+    console.log('nao existe')
+  }
+}
   
-  searchInput.forEach((element) => {
-    element.addEventListener("keydown", (event) => {
-      if(event.keyCode === 13){
-        const searchValue = event.target.value
-        alert(searchValue)
-      }
-    })
-  })
+  // searchInput.forEach((element) => {
+  //   element.addEventListener("keydown", (event) => {
+  //     if(event.keyCode === 13){
+  //       const searchValue = event.target.value
+  //       alert(searchValue)
+  //     }
+  //   })
+  // })
   
+
+
 
