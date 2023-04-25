18430e2f7ebae43251ed6a5e41c4239fa95ed2f4
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-24 23:28:40 -0300

    salvamento de rotina

--- javascript/searchFunction.js
+++ javascript/searchFunction.js
@@ -21,6 +21,14 @@ searchIcon.addEventListener("click", () => {
 //     }
 //   })
 // })
+const pages = {
+  'pão de queijo': '../pages/recipes/cheese-bread.html',
+  'pao de queijo': '../pages/recipes/cheese-bread.html',
+  'cheese bread': '../pages/recipes/cheese-bread.html',
+
+  'file de peixe': '../pages/recipes/fish-fillet.html',
+  'fish fillet': '../pages/recipes/fish-fillet.html'
+}
 
 searchInput.forEach((input) =>{
   input.addEventListener('input', search)
@@ -34,14 +42,7 @@ function search(){
   });
 
 
-  const pages = {
-    'pão de queijo': '../pages/recipes/cheese-bread.html',
-    'pao de queijo': '../pages/recipes/cheese-bread.html',
-    'cheese bread': '../pages/recipes/cheese-bread.html',
 
-    'file de peixe': '../pages/recipes/fish-fillet.html',
-    'fish fillet': '../pages/recipes/fish-fillet.html'
-  }
   const url = pages[query]
 
   if(url){
@@ -72,6 +73,33 @@ function search(){
 // const cheeseBread = new Recipes('Pão de queijo', 1, '../pages/recipes/cheese-bread.html')
 
 // console.log(cheeseBread)
+
+
+const divSuggestions = document.querySelector('.div-suggestions')
+
+searchInput.forEach((input) =>{
+  input.addEventListener('input', showSuggestions )
+})
+
+function showSuggestions(){
+  divSuggestions.classList.remove('d-none')
+  divSuggestions.classList.remove('hidden')
+
+  const span1 = document.createElement('span')
+  span1.textContent = 'abacate'
+
+  if(searchInput){
+
+  divSuggestions.appendChild(span1)
+  }
+
+  // let query = '';
+  // searchInput.forEach((input) => {
+  //   query += input.value.toLowerCase();
+  // });
+  // const filterRecipes = pages.filter(page => page.name.toLowerCase().includes(query))
+
+}
 
 
 
