ff0e4b4b436215bd745e1bbf9ad6ed34801b7a3b
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-25 23:44:41 -0300

    sem grandes progressos hoje

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -3,58 +3,50 @@ export { navbar, searchBarIcon }
 const body = document.querySelector('body')
 const btnMenu = document.querySelector('#btn-menu')
 const btnClose = document.querySelector('#btn-close')
-const navbarOffcanvas = document.querySelector('#navbar-offcanvas')
 const logo = document.querySelector('#logo')
-const categoryOffcanvas = document.querySelector('#category-offcanvas')
-const dropdownOffcanvas = document.querySelector('#dropdown-category-offcanvas')
+const linksNavbar = document.querySelector('#links-navbar')
 
 function navbar(){
   btnMenu.addEventListener('click',(ev) => {
     ev.preventDefault()
       btnMenu.classList.add('none')
       btnClose.classList.remove('none')
-      navbarOffcanvas.classList.remove('hidden-offcanvas')
-      navbarOffcanvas.classList.add('show-offcanvas')
-      logo.classList.add('none')
       body.classList.add('no-scroll')
+      linksNavbar.classList.remove('hidden-offcanvas')
+      linksNavbar.classList.add('show-offcanvas')
   })
 
   btnClose.addEventListener('click',(ev) => {
     ev.preventDefault()
     btnMenu.classList.remove('none')
     btnClose.classList.add('none')
-    navbarOffcanvas.classList.add('hidden-offcanvas')
-    navbarOffcanvas.classList.remove('show-offcanvas')
     logo.classList.remove('none')
+    linksNavbar.classList.remove('show-offcanvas')
+    linksNavbar.classList.add('hidden-offcanvas')
 
-    dropdownOffcanvas.classList.add('none')
-    dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
 
     body.classList.remove('no-scroll')
   })
 
-  categoryOffcanvas.addEventListener('click', (ev) => {
-    ev.preventDefault()
-    if(dropdownOffcanvas.classList.contains('none')){
-      dropdownOffcanvas.classList.remove('none')
-      dropdownOffcanvas.classList.add('dropdown-category-offcanvas')
-    } else{
-      dropdownOffcanvas.classList.add('none')
-      dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
-    }
-  })
+  // categoryOffcanvas.addEventListener('click', (ev) => {
+  //   ev.preventDefault()
+  //   if(dropdownOffcanvas.classList.contains('none')){
+  //     dropdownOffcanvas.classList.remove('none')
+  //     dropdownOffcanvas.classList.add('dropdown-category-offcanvas')
+  //   } else{
+  //     dropdownOffcanvas.classList.add('none')
+  //     dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
+  //   }
+  // })
 
   window.addEventListener('resize', function() {
     const screen = window.innerWidth
     if(screen > 992){
       btnMenu.classList.remove('none')
       btnClose.classList.add('none')
-      navbarOffcanvas.classList.add('hidden-offcanvas')
-      navbarOffcanvas.classList.remove('show-offcanvas')
       logo.classList.remove('none')
-
-      dropdownOffcanvas.classList.add('none')
-      dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
+      linksNavbar.classList.remove('hidden-offcanvas')
+      linksNavbar.classList.add('show-offcanvas')
     }
 
     
