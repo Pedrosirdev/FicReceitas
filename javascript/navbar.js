8aedad8abeef4a0a85d224c48736455e32717b48
Pedro Henrique <pedrosirdev@gmail.com>
2023-04-26 11:11:43 -0300

    Refazendo navbar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,65 +1,65 @@
-export { navbar, searchBarIcon }
+// export { navbar, searchBarIcon }
 
-const body = document.querySelector('body')
-const btnMenu = document.querySelector('#btn-menu')
-const btnClose = document.querySelector('#btn-close')
-const logo = document.querySelector('#logo')
-const linksNavbar = document.querySelector('#links-navbar')
+// const body = document.querySelector('body')
+// const btnMenu = document.querySelector('#btn-menu')
+// const btnClose = document.querySelector('#btn-close')
+// const logo = document.querySelector('#logo')
+// const linksNavbar = document.querySelector('#links-navbar')
 
-function navbar(){
-  btnMenu.addEventListener('click',(ev) => {
-    ev.preventDefault()
-      btnMenu.classList.add('none')
-      btnClose.classList.remove('none')
-      body.classList.add('no-scroll')
-      linksNavbar.classList.remove('hidden-offcanvas')
-      linksNavbar.classList.add('show-offcanvas')
-  })
+// function navbar(){
+//   btnMenu.addEventListener('click',(ev) => {
+//     ev.preventDefault()
+//       btnMenu.classList.add('none')
+//       btnClose.classList.remove('none')
+//       body.classList.add('no-scroll')
+//       linksNavbar.classList.remove('hidden-offcanvas')
+//       linksNavbar.classList.add('show-offcanvas')
+//   })
 
-  btnClose.addEventListener('click',(ev) => {
-    ev.preventDefault()
-    btnMenu.classList.remove('none')
-    btnClose.classList.add('none')
-    logo.classList.remove('none')
-    linksNavbar.classList.remove('show-offcanvas')
-    linksNavbar.classList.add('hidden-offcanvas')
+//   btnClose.addEventListener('click',(ev) => {
+//     ev.preventDefault()
+//     btnMenu.classList.remove('none')
+//     btnClose.classList.add('none')
+//     logo.classList.remove('none')
+//     linksNavbar.classList.remove('show-offcanvas')
+//     linksNavbar.classList.add('hidden-offcanvas')
 
 
-    body.classList.remove('no-scroll')
-  })
+//     body.classList.remove('no-scroll')
+//   })
 
-  // categoryOffcanvas.addEventListener('click', (ev) => {
-  //   ev.preventDefault()
-  //   if(dropdownOffcanvas.classList.contains('none')){
-  //     dropdownOffcanvas.classList.remove('none')
-  //     dropdownOffcanvas.classList.add('dropdown-category-offcanvas')
-  //   } else{
-  //     dropdownOffcanvas.classList.add('none')
-  //     dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
-  //   }
-  // })
+//   // categoryOffcanvas.addEventListener('click', (ev) => {
+//   //   ev.preventDefault()
+//   //   if(dropdownOffcanvas.classList.contains('none')){
+//   //     dropdownOffcanvas.classList.remove('none')
+//   //     dropdownOffcanvas.classList.add('dropdown-category-offcanvas')
+//   //   } else{
+//   //     dropdownOffcanvas.classList.add('none')
+//   //     dropdownOffcanvas.classList.remove('dropdown-category-offcanvas')
+//   //   }
+//   // })
 
-  window.addEventListener('resize', function() {
-    const screen = window.innerWidth
-    if(screen > 992){
-      btnMenu.classList.remove('none')
-      btnClose.classList.add('none')
-      logo.classList.remove('none')
-      linksNavbar.classList.remove('hidden-offcanvas')
-      linksNavbar.classList.add('show-offcanvas')
-    }
+//   window.addEventListener('resize', function() {
+//     const screen = window.innerWidth
+//     if(screen > 992){
+//       btnMenu.classList.remove('none')
+//       btnClose.classList.add('none')
+//       logo.classList.remove('none')
+//       linksNavbar.classList.remove('hidden-offcanvas')
+//       linksNavbar.classList.add('show-offcanvas')
+//     }
 
     
-  })
+//   })
 
 
-}
+// }
 
-function searchBarIcon(){
-  let searchBarIcon = document.querySelector('.search-bar-icon')
-  let searchContainer = document.querySelector('.search-container')
+// function searchBarIcon(){
+//   let searchBarIcon = document.querySelector('.search-bar-icon')
+//   let searchContainer = document.querySelector('.search-container')
 
-  searchBarIcon.addEventListener('click', () => {
-  searchContainer.classList.toggle('active');
-  })
-}
+//   searchBarIcon.addEventListener('click', () => {
+//   searchContainer.classList.toggle('active');
+//   })
+// }
